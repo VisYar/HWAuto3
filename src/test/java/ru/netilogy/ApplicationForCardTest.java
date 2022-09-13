73c2627531f3fd4e6a65d1acfac1f0ae37f1bdf3
Tatyana Dmitrieva <visyar76@gmail.com>
2022-09-13 12:28:56 +0300

    run

--- src/test/java/ru/netilogy/ApplicationForCardTest.java
+++ src/test/java/ru/netilogy/ApplicationForCardTest.java
@@ -1,9 +1,6 @@
 package ru.netilogy;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
